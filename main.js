da26112c9c00b76f089a641f8c096b9ca14ae52a
Ronald Kauan <kauanpaz49@gmail.com>
2021-06-29 11:46:29 -0400

    Adiicionei o footer

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   
+  mousewheel: true,
   keyboard: true,
 });
 
@@ -62,5 +63,21 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social,
 `,  {interval: 100} ) 
+
+/* Botão voltar para o topo  */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+window.addEventListener('scroll', function () {
+  if(window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+    console.log('SHOW')
+  }
+  else{
+    backToTopButton.classList.remove('show')
+    console.log('SEM SHOW')
+  }
+})
