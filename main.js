865af375836b2cef53f73d6ea8178cb6212e232e
Ronald Kauan <kauanpaz49@gmail.com>
2021-07-23 09:27:01 -0400

    Nao sei

--- main.js
+++ main.js
@@ -47,6 +47,12 @@ const swiper = new Swiper('.swiper-container', {
   
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView:2,
+      setWrapperSize: true
+    }
+  }
 });
 
 /* ScrollReview: Mostrar elementos quando der scroll na página */
